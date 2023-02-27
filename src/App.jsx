aea7294d522bcf9582a663575823b3dad3c61998
renato <rprobstsussoni@gmail.com>
2023-02-27 18:52:49 -0500

    add basename to browserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename="/shopping__cart">
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
